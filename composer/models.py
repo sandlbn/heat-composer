85088814bd9ddfdbb1d0a3e1f01d97cbe6a9c376
Marcin Spoczynski <marcinx.spoczynski@intel.com>
2015-06-06 19:27:30 +0100

    Added necessary db classes

--- composer/models.py
+++ composer/models.py
@@ -1,4 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class HeatTemplate(models.Model):
+    """
+    Heat Template class, all values except description and name are required
+    """
+    name = models.CharField(name="Name", max_length=255)
+    description = models.TextField(name="Description")
+    default_flavor = models.CharField(name="Default flavor for VMs", blank=True)
+    default_image = models.CharField(name="Default image for VMs", blank=True)
+    default_private_network = models.UUIDField(blank=True)
+    default_private_subnet = models.UUIDField(blank=True)
+    default_public_network = models.UUIDField(blank=True)
+    default_key = models.CharField(name="Default Keypair", blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class SecurityGroup(models.Model):
+    name = models.CharField(name="Name")
+
+    def __unicode__(self):
+        return self.name
+
+
+class VM(models.Model):
+    """
+    Vm class, all values except description and name are required
+    """
+    template = models.ForeignKey(HeatTemplate)
+    security_group = models.ForeignKey(SecurityGroup, blank=True)
+    name = models.CharField(name="Name", max_length=255)
+    description = models.TextField(name="Description")
+    flavor = models.CharField(name="Flavor for VMs", blank=True)
+    image = models.CharField(name="Image for VMs", blank=True)
+    private_network = models.UUIDField(blank=True)
+    private_subnet = models.UUIDField(blank=True)
+    public_network = models.UUIDField(blank=True)
+    key = models.CharField(name="Keypair", max_length=255)
+    script = models.TextField(name="Deployment script")
+
+    def __unicode__(self):
+        return self.name
+
 
--- composer/views.py
+++ composer/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from models import HeatTemplate, VM
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
+        return login_required(view)
+
+
+class TemplatesList(ListView, LoginRequiredMixin):
+    queryset = HeatTemplate.objects.order_by('-create_date')
+
+
+class TemplateCreate(CreateView, LoginRequiredMixin):
+    model = HeatTemplate
+
+
+class TemplateUpdate(UpdateView, LoginRequiredMixin):
+    model = HeatTemplate
+
+
+class TemplateDetail(DetailView, LoginRequiredMixin):
+    model = HeatTemplate
+
+
+class TemplateDelete(DeleteView, LoginRequiredMixin):
+    model = HeatTemplate
+    success_url = reverse_lazy('template-list')
